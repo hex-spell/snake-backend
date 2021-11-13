542043ae59918ee739666ff7097586ac2e4507f1
Modulariz <agustin.favo@gmail.com>
2021-11-13 09:54:50 -0300

    snake backend wip

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    application
+}
+
+group = "me.modulariz"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+}
+
+dependencies {
+    implementation("mysql:mysql-connector-java:8.0.11")
+    implementation("org.ktorm:ktorm-core:3.2.0")
+    implementation("org.ktorm:ktorm-support-mysql:3.2.0")
+    implementation("io.ktor:ktor-serialization:1.6.4")
+    implementation("com.google.code.gson:gson:2.8.9")
+    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-netty:1.6.0")
+    implementation("io.ktor:ktor-html-builder:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("ServerKt")
+}
---
+++ src/main/kotlin/Server.kt
@@ -0,0 +1,71 @@
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import kotlinx.serialization.json.Json
+import org.ktorm.database.Database
+import org.ktorm.dsl.*
+import org.ktorm.schema.Table
+import org.ktorm.schema.datetime
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
+
+
+object Players : Table<Nothing>("players") {
+    val id = int("id").primaryKey()
+    val username = varchar("username")
+    val points = int("points")
+    val saved_at = datetime("saved_at")
+}
+
+data class Player(val id: Int?,val username: String?,val points: Int?,val saved_at: String?)
+
+fun Application.module() {
+
+    val database = Database.connect(
+        url = "jdbc:mysql://localhost:3306/snake",
+        user = "root",
+        password = "root"
+    )
+
+    val builder = GsonBuilder()
+
+    builder.serializeNulls()
+
+    val gsonSerializer = builder.create()
+    val gsonDeSerializer = Gson()
+
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
+    }
+    install(Routing) {
+        get("/") {
+            var data: MutableList<Player> = ArrayList();
+            for (row in database.from(Players).select().limit(0, 10).orderBy(Players.points.desc())) {
+                data.add(Player(id=row[Players.id], username = row[Players.username], points= row[Players.points], saved_at = row[Players.saved_at].toString()))
+            }
+            val jsonData = gsonSerializer.toJson(data)
+            call.respondText(jsonData, ContentType.Application.Json, HttpStatusCode.OK)
+        }
+        post("/") {
+            val requestBody = call.receiveText();
+            val parsedRequestBody = gsonDeSerializer.fromJson<Player>(requestBody, Player::class.java);
+            val jsonData = gsonSerializer.toJson(parsedRequestBody)
+            call.respondText(jsonData, ContentType.Application.Json, HttpStatusCode.OK)
+        }
+    }
+}
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module).start(wait = true)
+}
